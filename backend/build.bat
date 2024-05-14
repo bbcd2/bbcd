4871b8bf7147a5ab27f3f1c444593b9a87a1c358
Kat <kat@kattschan.co.uk>
2024-05-14 11:00:56 -0400

    okay i really hope this is the last time this happens

--- backend/build.bat
+++ backend/build.bat
@@ -2,6 +2,6 @@
 REM Zip up download.js, index.js, package.json, and .env into ../output/backend.zip
 mkdir ..\output
 mkdir ..\output\backend
-copy package.json backend
-copy download.js backend
-copy index.js backend
+copy package.json ..\output\backend
+copy download.js ..\output\backend
+copy index.js ..\output\backend
