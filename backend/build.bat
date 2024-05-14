e308b58d50b550560ef07e54537e69e01f32e833
--global <--global>
2024-05-14 10:50:10 -0400

    update the build.bats

--- backend/build.bat
+++ backend/build.bat
@@ -1,12 +1,8 @@
 @ECHO OFF
 REM Zip up download.js, index.js, package.json, and .env into ../output/backend.zip
-mkdir temp
 mkdir ..\output
-copy package.json temp
-copy download.js temp
-copy index.js temp
-copy .env temp
-cd temp
-powershell.exe Compress-Archive -Path * -DestinationPath ..\..\output\backend.zip
-cd ..
-rmdir /s /q temp
+mkdir ..\output\backend
+copy package.json backend
+copy download.js backend
+copy index.js backend
+copy .env backend
--- frontend/build.bat
+++ frontend/build.bat
@@ -1,12 +1,9 @@
 @echo off
 
-REM Create temp folder
-mkdir temp
+
 REM create output folder
 mkdir ..\output
+mkdir ..\output\frontend
 REM copy the build directory and the package.json file to the temp folder
-xcopy /E /I build temp
-copy package.json temp
-REM zip the contents of the temp folder into ../output/backend.zip
-cd temp
-powershell.exe Compress-Archive -Path * -DestinationPath ..\..\output/frontend.zip
+xcopy /E /I build ..\output\frontend
+copy package.json ..\output\frontend
